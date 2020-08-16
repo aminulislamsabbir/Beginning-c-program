dcc22e3d918a552907190b547efd2629d643eb10
Aminul Isalm Sabbir <aminulislam.sabbir98@gmail.com>
2020-08-17 02:57:19 +0600

    Even and odd number

--- even_odd.c
+++ even_odd.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
-
 /* problem link:   https://dimikoj.com/problems/1/52-problem-book-odd-even-1-by-dimik-computing */
 
 int main () {
     int T,n;
-    
+
     scanf("%d",&T);
 
     for(int i = 1; i<=T; i++) {
         scanf("%d",&n);
-
         if (n % 2 == 0) {
             printf("Even : %d\n",n);
         } else {
